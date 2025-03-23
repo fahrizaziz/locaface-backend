40fc4145fd5d6564f96aa56ccc054b7ec8ceb06e
fahriz aziz <azizfahriz@gmail.com>
2025-03-23 17:42:45 +0700

    Import Stisla Template, Auth with Fortify, User Management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+    // Route::resource('companies', CompanyController::class);
+    // Route::resource('attendances', AttendanceController::class);
+    // Route::resource('permissions', PermissionController::class);
 });
